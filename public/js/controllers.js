399ae003a4b3673560838c53a10c33f1fd4bfc5d
Nicholas Gallardo <nic.gallardo@gmail.com>
2015-12-18 21:44:30 -0700

    works with api call to nasdaq

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,8 +2,27 @@ app.controller('MainController', function($scope, $http){
 
 })
 
-app.controller('StockController', function($scope, $http){
-  $scope.message = "Stock"
+app.controller('StockController', function($scope, $http, $routeParams){
+  $scope.message = "Stock";
+  $scope.currentParam = $routeParams;
+  var symbol = $routeParams.symbol;
+
+  var d = new Date();
+  var dd = d.getDate();
+  var mm = d.getMonth()+1;
+  var yyyy = d.getFullYear();
+  $scope.currentDate  = yyyy + "-" + mm + "-" + dd;
+
+  $http({
+  method: 'GET',
+  url: 'https://www.quandl.com/api/v3/datasets/WIKI/' + symbol + '.json?end_date=' + $scope.currentDate + '&api_key=uQpkcEhcJnu1NxsUesWv'
+  }).then(function successCallback(response) {
+    console.log(response.data.dataset.data);
+  })
+
+  //get stock info by dates                     ((SYMBOL))                                    ((time >>> daily or quarterly >>))((DATES to lookup))
+//https://www.quandl.com/api/v3/datasets/WIKI/AAPL.json?column_index=4&order=asc&collapse=daily&start_date=2012-01-01&end_date=2015-12-01&api_key=uQpkcEhcJnu1NxsUesWv
+
 })
 
 app.controller('StocksController', function($scope, $http){
@@ -13,10 +32,13 @@ app.controller('StocksController', function($scope, $http){
   method: 'GET',
   url: '/symbols.json'
   }).then(function successCallback(response) {
-    $scope.allCompanies = response;
-    console.log(response);
-
+    $scope.allCompanies = response.data;
   })
+
+  $scope.go = function() {
+
+    $scope.msg = 'clicked';
+  }
 })
 
 app.controller('AboutController', function($scope, $http){
