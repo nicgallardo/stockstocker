187ac831b3782bcfb3b76375affbb29bdfafbc3e
Nicholas Gallardo <nic.gallardo@gmail.com>
2015-12-19 00:45:33 -0700

    added simple horizontal bar chart using directive

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,3 +1,5 @@
+var foo = "FUCK ALL";
+
 app.controller('MainController', function($scope, $http){
 
 })
@@ -6,19 +8,19 @@ app.controller('StockController', function($scope, $http, $routeParams){
   $scope.message = "Stock";
   $scope.currentParam = $routeParams;
   var symbol = $routeParams.symbol;
-
+  $scope.bar;
   var d = new Date();
   var dd = d.getDate();
   var mm = d.getMonth()+1;
   var yyyy = d.getFullYear();
   $scope.currentDate  = yyyy + "-" + mm + "-" + dd;
-
-  $http({
-  method: 'GET',
-  url: 'https://www.quandl.com/api/v3/datasets/WIKI/' + symbol + '.json?end_date=' + $scope.currentDate + '&api_key=uQpkcEhcJnu1NxsUesWv'
-  }).then(function successCallback(response) {
-    console.log(response.data.dataset.data);
-  })
+//WORKS
+  // $http({
+  // method: 'GET',
+  // url: 'https://www.quandl.com/api/v3/datasets/WIKI/' + symbol + '.json?end_date=' + $scope.currentDate + '&api_key=uQpkcEhcJnu1NxsUesWv'
+  // }).then(function successCallback(response) {
+  //   $scope.bar = response.data.dataset.data;
+  // })
 
   //get stock info by dates                     ((SYMBOL))                                    ((time >>> daily or quarterly >>))((DATES to lookup))
 //https://www.quandl.com/api/v3/datasets/WIKI/AAPL.json?column_index=4&order=asc&collapse=daily&start_date=2012-01-01&end_date=2015-12-01&api_key=uQpkcEhcJnu1NxsUesWv
